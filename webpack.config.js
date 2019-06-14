bc0a969912118d4d3e8c9e1d4ac9cb06cf019e40
jnlmendonca <jnl.mendonca@gmail.com>
2019-06-14 15:38:14 +0100

    Add standard linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-var path = require('path');
+var path = require('path')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname),
-        libraryTarget: 'assign',
-        library: 'DangerNoodle',
-        libraryExport: 'default',
-    },
-    module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ]
-        }]
-    },
-};
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname),
+    libraryTarget: 'assign',
+    library: 'DangerNoodle',
+    libraryExport: 'default'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  }
+}
